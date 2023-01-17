58c67e1800876d318b0c615b83b33d5892bcd846
Yeison Andres Rojas Asencio <yrojas@lucasian.com>
2023-01-17 11:31:21 -0500

    Datos Comunes

---
+++ src/datos/DatosComunes.java
@@ -0,0 +1,19 @@
+package datos;
+
+public class DatosComunes {
+    public static void main(String[] args) {
+
+        int number = 6;
+        long number2 = 5;
+        double number3 = 5.6;
+        boolean dec = true;
+        String name = "Camilo";
+
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(dec);
+        System.out.println(name);
+
+    }
+}
